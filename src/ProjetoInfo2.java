a759d7602e89160f8431bfe16ce1bb886b74a5ca
Maicon <Maicon@DESKTOP-V6AUOBF>
2020-03-30 16:27:43 -0300

    Meu primeiro commit (maicon)

---
+++ src/ProjetoInfo2.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author Maicon
+ */
+public class ProjetoInfo2 {
+      public static void main(String[] args) {
+        System.out.println("Teste para enviar pro github");
+        System.out.println("Maicon, Kéven e Matheus");
+        
+        int a = 4;
+        int b = 5;
+        
+        System.out.println(a+b);
+    }
+}
